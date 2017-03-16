c37a979a09806bf4a127d18e1bd2d7c11e75abb9
LuisEGR <luisegr149@gmail.com>
2017-03-16 13:57:05 -0600

    Rutas corregidas

--- paginas/add_permiso.php
+++ paginas/add_permiso.php
@@ -19,7 +19,7 @@ include '../../conf.php'
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="<?php echo $url_server; ?>/lib/ie10-viewport-bug-workaround.css" rel="stylesheet">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/css/bootstrap-datepicker.min.css" rel="stylesheet">	
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/css/bootstrap-datepicker.min.css" rel="stylesheet">
 
 
 
@@ -32,24 +32,24 @@ include '../../conf.php'
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-	
+
 	<script type="text/javascript" src="<?php echo $url_server; ?>/lib/angular-1.5.8/angular.min.js"></script>
 	<script type="text/javascript" src="../../js/add_permiso.js"></script>
-	
+
   </head>
 
   <body ng-controller='AddPuestoCntroller'>
 
     <div class="container-fluid">
       <div class="row">
-	  
-		
+
+
 		<div >
-			<?php include_once "../../views/permisos/add.php"; ?>
-		
+			<?php include_once "views/permisos/add.php"; ?>
+
 		</div>
-		
-        		
+
+
 
 
         </div>
@@ -64,7 +64,7 @@ include '../../conf.php'
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.0/js/bootstrap-datepicker.min.js"></script>
 <script>
     $('.datepick').datepicker({
-	
+
 	        format: "yyyy-mm-dd",
         autoclose: true,
         todayHighlight: true
--- views/permisos/permisos.php
+++ views/permisos/permisos.php
@@ -1,13 +1,13 @@
 
         <div class="col-sm-9 col-sm-offset-3 col-md-12 col-md-offset-0 main">
 			<h1 class="page-header">Permisos</h1>
-			
+
 			<button type="button" class="btn btn-default" onclick="location.href='add_permiso.php'" data-dismiss="modal">Agregar permiso</button><br><br>
-			
-			
-			<input type="text" ng-change="load(1, nameSearch)" ng-model="nameSearch" class="form-control" placeholder="Buscar..." 
+
+
+			<input type="text" ng-change="load(1, nameSearch)" ng-model="nameSearch" class="form-control" placeholder="Buscar..."
 			ng-model-options='{ debounce: 300 }'/>
-			
+
 			<div class="table-responsive">
             <table class="table table-striped">
               <thead>
@@ -26,7 +26,7 @@
                   <td>
 						{{ m.permiso_key || "Vacio" }}
 				  </td>
-				  <td>				  
+				  <td>
 						<a href="#" editable-select="m.group" onshow="loadGroups()" e-ng-options="g.id_grupo as g.grupo for g in groups" onaftersave="updateGroup(m)" >
 							{{ m.groupName || 'not set' }}
 						</a>
@@ -38,16 +38,13 @@
 				  </td>
 				  <td><a href="#" editable-text="m.permiso_detalles" onaftersave="updateDetalle(m)" >{{ m.permiso_detalles || "Vacio" }}</a></td>
                   <td>{{m.fecha_agregado}}</td>
-				  
+
                 </tr>
 
               </tbody>
             </table>
           </div>
-	
-	<div style='float: right;'>
-		<?php include_once '../views/general/paginacion.php'; ?>
-	</div>
 
-  
-  
+	<div style='float: right;'>
+		<?php include_once 'views/general/paginacion.php'; ?>
+	</div>
