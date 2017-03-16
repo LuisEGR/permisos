d2a7d34018b2a256214184cb517adb30bf91136a
LuisEGR <luisegr149@gmail.com>
2017-03-16 11:38:20 -0600

    Corrección de rutas y menú agregado

--- agregar/index.php
+++ agregar/index.php
@@ -1,6 +1,5 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/menu/Menu.class.php";
 Menu::start("Administración de Permisos", 'menu_Admin');
-include_once "../paginas/permisos.php";
-
+include_once "../../paginas/add_permiso.php";
 ?>
