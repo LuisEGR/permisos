6bb72e2cb7fccacf91b172caa9e612d79c9daf82
LuisEGR <luisegr149@gmail.com>
2017-03-20 21:16:37 -0600

    Ruta corregida

--- api/getUsers.php
+++ api/getUsers.php
@@ -1,5 +1,5 @@
-<?php 
-  include_once $_SERVER['DOCUMENT_ROOT']."/sistemas/class/Permisos.class.php";
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Permisos.class.php";
   include_once $_SERVER['DOCUMENT_ROOT']."/sistema/api/v1/base.api.php";
 
   Response::$data->result = Permisos::getAccesos();
