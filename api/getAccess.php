448ae78e24459982789554b6637088862e6bccfd
LuisEGR <luisegr149@gmail.com>
2017-03-20 21:19:41 -0600

    Api para traer todos los accesos

---
+++ api/getAccess.php
@@ -0,0 +1,7 @@
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Permisos.class.php";
+  include_once $_SERVER['DOCUMENT_ROOT']."/sistema/api/v1/base.api.php";
+
+  Response::$data->result = Permisos::getAllAccess();
+  Response::showResult();
+?>
