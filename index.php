a7c15c7753198de78d5cf22b2f399dec1191074b
LuisEGR <luisegr149@gmail.com>
2017-03-21 13:32:48 -0600

    Cambio de dirección de template

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/menu/Menu.class.php";
 Menu::start("Administración de Permisos", 'menu_Admin');
-include_once '/templates/index.template.php';
+include_once '/templates/index.template.html';
 Menu::end();
 ?>
