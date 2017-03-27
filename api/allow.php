c53c08e51173cc1f35697b2d6a188594049480a1
LuisEGR <luisegr149@gmail.com>
2017-03-27 10:03:14 -0600

    Conceder accesos

--- api/allow.php
+++ api/allow.php
@@ -1,9 +1,8 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Permisos.class.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/sistema/api/v1/base.api.php";
-$uid = $_POST['uid'];
-$pid = $_POST['pid'];//1-set, 0-deny
-if(!isset($_POST['uid'], $_POST['pid'])){
+$data = json_decode(file_get_contents('php://input'));
+if( !isset($data->uid, $data->pid) ){
   Response::noDataResponse();
 }
 Response::$data->result = Permisos::Allow($uid, $pid, true);
--- api/deny.php
+++ api/deny.php
@@ -1,10 +1,8 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT']."/sistema/class/Permisos.class.php";
 include_once $_SERVER['DOCUMENT_ROOT']."/sistema/api/v1/base.api.php";
-$uid = $_POST['uid'];
-$pid = $_POST['pid'];
-$type = $_POST['type']; //1-set, 0-deny
-if(!isset($_POST['uid'], $_POST['pid'], $_POST['type'])){
+$data = json_decode(file_get_contents('php://input'));
+if( !isset($data->uid, $data->pid) ){
   Response::noDataResponse();
 }
 Response::$data->result = Permisos::Deny($uid, $pid, true);
