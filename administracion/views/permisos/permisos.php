4868600287ac1f1ba01cc0d81e1d65b549e82d7a
jonatanestrada <jonatan.estrada@hovahealth.com>
2017-03-27 09:23:27 -0600

    Se cambio el boton de agregar permiso

--- administracion/views/permisos/permisos.php
+++ administracion/views/permisos/permisos.php
@@ -2,8 +2,7 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-12 col-md-offset-0 main">
 			<h1 class="page-header">Permisos</h1>
 
-			<button type="button" class="btn btn-default" onclick="location.href='agregar'" data-dismiss="modal">Agregar permiso</button><br><br>
-			<img src='/img/door-key.png' ng-click='getDataAddPermiso(m)' data-toggle="modal" data-target="#modalAddPermiso" class='btnImg' style='float: right;padding: 10px;cursor:pointer;'><br><br>
+			<img src='/img/door-key.png' onclick="location.href='agregar'"  class='btnImg' style='float: right;padding: 10px;cursor:pointer;'><br><br>
 
 			<input type="text" ng-change="load(1, nameSearch)" ng-model="nameSearch" class="form-control" placeholder="Buscar..."
 			ng-model-options='{ debounce: 300 }'/>
@@ -16,8 +15,8 @@
                 <tr>
                   <th>ID</th>
                   <th>Key</th>
-				  <th>Grupo <a href='' >+</a></th>
-				  <th>P&aacute;gina <a href='' ng-click='getGrupos()' data-toggle="modal" data-target="#modalAddPage">+</a></th>
+				  <th>Grupo</th>
+				  <th>P&aacute;gina </th>
 				  <th>Detalles</th>
                   <th>Fecha Agregado</th>
                 </tr>
